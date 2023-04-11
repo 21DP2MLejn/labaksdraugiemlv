ff9840ac1d9ae35877f770d9ad2311b58dca33ff
21DP2JFern <12djfernats@rvt.lv>
2023-04-11 17:53:15 +0300

    edit

---
+++ editinfo.js
@@ -0,0 +1,71 @@
+function editnickbox(){
+
+    var text = document.getElementById("edit-nick");
+    var editbut = document.getElementById("edit-nick-but");
+    var accbut = document.getElementById("acc-nick-but");
+
+    if (text.style.display === "none"){
+        text.style.display = "block";
+        editbut.style.display = "none";
+        accbut.style.display = "block";
+
+    } else{
+        text.style.display = "none";
+        editbut.style.display = "block";
+        accbut.style.display = "none";
+    }
+}
+
+function editnamebox(){
+
+    var text = document.getElementById("edit-name");
+    var editbut = document.getElementById("edit-name-but");
+    var accbut = document.getElementById("acc-name-but");
+
+    if (text.style.display === "none"){
+        text.style.display = "block";
+        editbut.style.display = "none";
+        accbut.style.display = "block";
+
+    } else {
+        text.style.display = "none";
+        editbut.style.display = "block";
+        accbut.style.display = "none";
+    }
+}
+
+function editbdaybox(){
+
+    var text = document.getElementById("edit-bday");
+    var editbut = document.getElementById("edit-bday-but");
+    var accbut = document.getElementById("acc-bday-but");
+
+    if (text.style.display === "none"){
+        text.style.display = "block";
+        editbut.style.display = "none";
+        accbut.style.display = "block";
+
+    } else{
+        text.style.display = "none";
+        editbut.style.display = "block";
+        accbut.style.display = "none";
+    }
+}
+
+function editcountrybox(){
+
+    var text = document.getElementById("edit-country");
+    var editbut = document.getElementById("edit-country-but");
+    var accbut = document.getElementById("acc-country-but");
+
+    if (text.style.display === "none"){
+        text.style.display = "block";
+        editbut.style.display = "none";
+        accbut.style.display = "block";
+
+    } else{
+        text.style.display = "none";
+        editbut.style.display = "block";
+        accbut.style.display = "none";
+    }
+}
